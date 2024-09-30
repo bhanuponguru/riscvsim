17f76e6854ec9c62efd20d69240b844938fe89d7
bhanuponguru <bhanuponguru@gmail.com>
2024-09-30 02:03:32 +0000

    fixed error in branch execution when imm is negative.

--- src/processor.cpp
+++ src/processor.cpp
@@ -95,19 +95,21 @@ void execute(int instruction, long long registers[], char memory[], int& pc, vec
     else if (opcodes[opcode] == "b") {
         int rs1 = (instruction>>15)&0b11111;
         int rs2 = (instruction>>20)&0b11111;
-        int imm = ((instruction>>31)&0b1)<<12 | ((instruction>>25)&0b111111)<<5 | ((instruction>>8)&0b1111)<<1 | ((instruction>>7)&0b1);
+        //in imm, last bit is ignored in the instruction because it is always 0.
+        int imm = (((instruction>>31)&0b1)<<12 | ((instruction>>7)&0b1)<<11 | ((instruction>>25)&0b111111)<<5 | ((instruction>>8)&0b1111)<<1) & 0b1111111111110;
+        imm=sign_extend(imm, 12);
         int funct3 = (instruction>>12)&0b111;
         switch (funct3) {
             case 0x0: // beq case.
                 if (registers[rs1] == registers[rs2]) {
                     pc += imm;
- cout << "executed instruction: " << lines[line_numbers[(current_pc/4)+1]-1] << " ; under pc: " << current_pc << " in line " << (current_pc/4)+1 << endl;
+ cout << "executed instruction: " << lines[line_numbers[(current_pc/4)]] << " ; under pc: " << current_pc << " in line " << (current_pc/4)+1 << endl;
                     return;
                 }
                 break;
         }
     }
  pc+=4;
- cout << "executed instruction: " << lines[line_numbers[(current_pc/4)+1]-1] << " ; under pc: " << current_pc << " in line " << (current_pc/4)+1 << endl;
+ cout << "executed instruction: " << lines[line_numbers[(current_pc/4)]] << " ; under pc: " << current_pc << " in line " << (current_pc/4)+1 << endl;
 }
 
--- test.s
+++ test.s
@@ -3,5 +3,5 @@ lui x6, 16
 sh x5, 0(x6)
 lh x7, 0(x6)
 beq x0, x0, l1
-l1: addi x5, x0, 1
+l1: addi x5, x5, 1
 beq x5, x0, l1
